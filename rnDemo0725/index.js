3cdea5ffff434b1988633e46403c4f5c2b017bac
yw.hao <yw.hao@trip.com>
2024-04-11 14:57:02 +0800

    feat: add Animated.scrollview measurelayout

--- rnDemo0725/index.js
+++ rnDemo0725/index.js
@@ -2,8 +2,44 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {Navigation} from 'react-native-navigation';
+import Home from './src/Home';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('Home', () => Home);
+Navigation.registerComponent(
+  'AnimatedOfMeasureLayout',
+  () => require('./src/pages/AnimatedOfMeasureLayout').default,
+);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
+
+Navigation.setDefaultOptions({
+  statusBar: {
+    backgroundColor: '#4d089a',
+  },
+  topBar: {
+    title: {
+      color: 'white',
+    },
+    backButton: {
+      color: 'white',
+    },
+    background: {
+      color: '#4d089a',
+    },
+  },
+});
---
+++ rnDemo0725/src/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+
+const Index = props => {
+  return (
+    <View style={styles.container}>
+      <Button
+        title="AnimatedOfMeasureLayoutInValid"
+        onPress={() =>
+          Navigation.push(props.componentId, {
+            component: {
+              name: 'AnimatedOfMeasureLayout',
+              passProps: {
+                text: 'Pushed screen',
+              },
+            },
+          })
+        }
+      />
+    </View>
+  );
+};
+
+Index.options = {
+  topBar: {
+    title: {
+      text: 'Home',
+      color: 'white',
+    },
+  },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Index;
